7ffed358e8769d41663af433ea40273fdb636559
Paul J <jorpaulvincent@gmail.com>
2022-07-22 22:16:13 +0800

    update navbar with vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,33 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+
+  data: () => ({
+    //
+  }),
+};
+</script>
+
 <style>
+* {
+  padding: 0;
+  margin: 0;
+  text-decoration: none;
+  list-style: none;
+  box-sizing: border-box;
+  font-family: "Lato", sans-serif;
+
+  text-transform: initial;
+}
+
 /* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -12,25 +35,14 @@
   text-align: center;
   color: #2c3e50;
 }
-
 #nav {
   padding: 30px;
 }
-
 #nav a {
   font-weight: bold;
   color: #2c3e50;
 }
-
 #nav a.router-link-exact-active {
   color: #42b983;
 } */
-
-* {
-  padding: 0;
-  margin: 0;
-  text-decoration: none;
-  list-style: none;
-  box-sizing: border-box;
-}
 </style>
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -27,14 +27,16 @@
         </router-link>
       </li>
       <div class="buttons">
-        <button>
-          <router-link :to="'/'">
-            <span>Log In</span>
-          </router-link>
-        </button>
-        <button>
-          <router-link :to="'/'"><span>Register</span></router-link>
-        </button>
+        <router-link :to="'/'">
+          <v-btn class="ma-2" outlined color="#0091DE">
+            <span class="login">Log In</span>
+          </v-btn>
+        </router-link>
+        <router-link :to="'/'">
+          <v-btn color="#0091DE">
+            <span class="register">Register</span>
+          </v-btn>
+        </router-link>
       </div>
     </ul>
   </header>
@@ -49,7 +51,6 @@ span {
   display: flex;
   justify-content: space-between;
 
-  font-family: "Lato", sans-serif;
   padding: 0px 5% 0px 5%;
   border-bottom: 1px solid #e0e0e0;
   box-shadow: 0px 0px 14px 0px rgba(0, 0, 0, 0.05);
@@ -77,14 +78,21 @@ span {
   margin: auto;
 }
 
-.main-nav button {
-  height: 45px;
+.main-nav .v-btn {
   margin: 5px;
   padding: 10px;
 }
 
-.main-nav button span {
+.main-nav .v-btn span {
   font-size: 1.2em;
+}
+
+.main-nav .v-btn span .login {
+  color: #0091de;
+}
+
+.main-nav .v-btn span .register {
+  color: #ffff;
 }
 
 .main-nav li:hover {
